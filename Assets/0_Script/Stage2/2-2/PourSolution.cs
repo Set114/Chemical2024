316980b7201d2516dec99aecd0b3001eb9a59306
Kevin0901 <j94191274@gmail.com>
2024-11-20 19:53:49 +0800

    分鏡2、3聲音文字OK

--- Assets/0_Script/Stage2/2-2/PourSolution.cs
+++ Assets/0_Script/Stage2/2-2/PourSolution.cs
@@ -1,12 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PourSolution : MonoBehaviour
 {
     [SerializeField] GameObject Liquid;
     [SerializeField] GameObject putpoint;
     [SerializeField] GameObject TestTube;
+    public AudioSource audioSource1;
+    public AudioSource audioSource2;
+    public AudioSource audioSource3;
+    public AudioSource audioSource4;
+    public AudioSource audioSource5;
+
+    public string[] Text;
+    public Text text;
     bool isputed = false;
     private void OnTriggerEnter(Collider other)
     {
@@ -17,7 +26,7 @@ public class PourSolution : MonoBehaviour
         else if (other.gameObject.name.Equals("TestTube") && TestTube.transform.Find("Liquid").gameObject.activeSelf)
         {
             isputed = true;          
-           
+            StartCoroutine(WaitForAudioToEnd());
         }
     }
 
@@ -28,6 +37,17 @@ public class PourSolution : MonoBehaviour
             TestTube.gameObject.transform.position = putpoint.transform.position;
             TestTube.gameObject.transform.rotation = putpoint.transform.rotation;
         }
+    }
+
+    private IEnumerator WaitForAudioToEnd()
+    {
+        audioSource1.Play(); // 播放音频
+        text.text = Text[0];
+
+        // 等待音频播放完成
+        yield return new WaitForSeconds(audioSource1.clip.length);
+
+        // audioSource2.Play();
     }
 
 }
---
+++ Assets/0_Script/Stage2/2-2/RotationStateChecker.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationStateChecker : MonoBehaviour
+{
+    public GameObject DefultLiquid;
+    public GameObject DefultLiquid2;
+    public GameObject AfterLiquid;
+    public GameObject image;
+    public enum RotationState
+    {
+        None,       // 无状态
+        Positive90, // 正向超出90度
+        Negative90  // 负向超出90度
+    }
+
+    public RotationState currentState = RotationState.None; // 当前状态
+    private bool hasTriggered = false; // 是否已经触发状态
+
+    void Update()
+    {
+        if (hasTriggered) return; // 如果已经触发过状态，跳过检测
+
+        // 获取物体的当前 Z 轴旋转角度
+        float zRotation = NormalizeAngle(transform.eulerAngles.z);
+
+        // 检查是否超出正负90度
+        if (zRotation > 90f)
+        {
+            SetState(RotationState.Positive90);
+        }
+        else if (zRotation < -90f)
+        {
+            SetState(RotationState.Negative90);
+        }
+    }
+
+    private void SetState(RotationState newState)
+    {
+        DefultLiquid.SetActive(false);
+        DefultLiquid2.SetActive(false);
+        AfterLiquid.SetActive(true);
+        image.SetActive(true);
+        currentState = newState;
+        hasTriggered = true; // 标记为已触发，避免重复
+        Debug.Log($"状态更新为: {currentState}");
+        GetComponentInChildren<PourSolution>().audioSource4.Play();
+        GetComponentInChildren<PourSolution>().text.text = GetComponentInChildren<PourSolution>().Text[3];
+    }
+
+    private float NormalizeAngle(float angle)
+    {
+        // 将角度规范化到 -180 到 180 范围
+        if (angle > 180f)
+        {
+            angle -= 360f;
+        }
+        return angle;
+    }
+}
