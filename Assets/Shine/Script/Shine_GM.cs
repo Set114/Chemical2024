5bf079c60002eb6d58ecf6795426e2c77e36150d
Shineliu77 <shinecookie@gmail.com>
2024-12-04 20:47:56 +0800

    加入教學4-5素材

---
+++ Assets/Shine/Script/Shine_GM.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shine_GM : MonoBehaviour
+{
+    public GameObject TestMode, TeachMode;
+    // Start is called before the first frame update
+    void Start()
+    {
+        switch (MenuUIManager.SharedChapterModeData) {
+            case 0:
+                TeachMode.SetActive(true);
+                break;
+            case 1:
+                TestMode.SetActive(true);
+                break;
+        }
+    }
+
+    public void SelectTestMode() {
+        MenuUIManager.SharedChapterModeData= 1;
+        Application.LoadLevel(Application.loadedLevel);
+    }
+    public void SelectTeachMode()
+    {
+        MenuUIManager.SharedChapterModeData = 0;
+        Application.LoadLevel(Application.loadedLevel);
+    }
+    public void BackMenu()
+    {
+        MenuUIManager.shouldOpenMenu = true;
+        Application.LoadLevel("MainMenu");
+    }
+}
