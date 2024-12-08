c5249a35f917a6ae85a3b88f246d478385306468
Shineliu77 <shinecookie@gmail.com>
2024-12-08 18:37:35 +0800

    教學6-1

--- Assets/Shine/Script/six/6-1/Water.cs
+++ Assets/Shine/Script/six/6-1/Water.cs
@@ -5,10 +5,20 @@ using UnityEngine;
 public class Water : MonoBehaviour
 {
     Material Watermaterial;
+    public GameObject[] ChemicalImage;
+    public Collider PotassiumCollider, AcidCollider;
+    public GameObject[] InfoUI;
+
+    public GameObject[] TackObjs;
+    public Vector3[] RecordObjs;
+    public GameObject Final;
     // Start is called before the first frame update
     void Start()
     {
-        
+        RecordObjs[0] = TackObjs[0].transform.position;
+        RecordObjs[1] = TackObjs[1].transform.position;
+        RecordObjs[2] = TackObjs[2].transform.position;
+
     }
 
     // Update is called once per frame
@@ -16,43 +26,90 @@ public class Water : MonoBehaviour
     {
         
     }
+    public void ReButton() {
+        TackObjs[0].transform.position = RecordObjs[0];
+        TackObjs[1].transform.position = RecordObjs[1];
+        TackObjs[2].transform.position = RecordObjs[2];
+
+        TackObjs[0].SetActive(true);
+        TackObjs[1].SetActive(true);
+        TackObjs[2].SetActive(true);
+
+        ChemicalImage[0].SetActive(false);
+        ChemicalImage[1].SetActive(false);
+        ChemicalImage[2].SetActive(false);
+
+        InfoUI[0].SetActive(false);
+        InfoUI[1].SetActive(false);
+        TackObjs[0].GetComponent<Collider>().enabled = true;
+        TackObjs[1].GetComponent<Collider>().enabled = false;
+        TackObjs[2].GetComponent<Collider>().enabled = false;
+
+        TackObjs[1].transform.localEulerAngles = Vector3.zero;
+        TackObjs[2].transform.localEulerAngles = Vector3.zero;
+
+        Watermaterial.SetColor("_SpecColor", Color.white);
+        Watermaterial.SetColor("_TopColor", Color.white);
+        Watermaterial.SetColor("_BottomColor", Color.white);
+        Watermaterial.SetColor("_EdgeColor", Color.white);
+        Watermaterial.SetColor("_GlowColor", Color.white);
+    }
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log(other.gameObject.tag);
         Debug.Log("layer"+other.gameObject.layer);
-        if (other.gameObject.tag == "yellowObj")
+        if (other.gameObject.name == "yellowObj")
         {
-            Debug.Log("碰到了黃色塊");
             Watermaterial = this.gameObject.GetComponent<Renderer>().material;
-
+            other.gameObject.SetActive(false);
             Watermaterial.SetColor("_SpecColor", Color.yellow);
             Watermaterial.SetColor("_TopColor", Color.yellow);
             Watermaterial.SetColor("_BottomColor", Color.yellow);
             Watermaterial.SetColor("_EdgeColor", Color.yellow);
             Watermaterial.SetColor("_GlowColor", Color.yellow);
-        }
-        Debug.Log(other.gameObject.tag);
-        if (other.gameObject.tag == "acid")
-        {
-            Debug.Log("碰到了鹽酸");
-            Material Auid= this.gameObject.GetComponent<Renderer>().material;
+            ChemicalImage[0].SetActive(true);
+            PotassiumCollider.enabled = true;
+            AcidCollider.enabled = true;
+            InfoUI[0].SetActive(true);
 
+        }
+        if (other.gameObject.name == "Acid")
+        {
+            Material Auid= this.gameObject.GetComponent<Renderer>().material;
+            other.gameObject.SetActive(false);
             Auid.SetColor("_SpecColor", new Color32(255,100,0,255));
             Auid.SetColor("_TopColor", new Color32(255, 100, 0, 255));
             Auid.SetColor("_BottomColor", new Color32(255, 100, 0, 255));
             Auid.SetColor("_EdgeColor", new Color32(255,100,0,255));
             Auid.SetColor("_GlowColor", new Color32(255, 100, 0, 255));
-        }
-        if (other.gameObject.tag == "potassium")
-        {
-            Debug.Log("碰到了鉀");
-            Material pota = this.gameObject.GetComponent<Renderer>().material;
+            ChemicalImage[1].SetActive(true);
+            ChemicalImage[0].SetActive(false);
+            ChemicalImage[2].SetActive(false);
+            StartCoroutine(FinalCheck());
 
-            pota.SetColor("_SpecColor", Color.yellow);
+        }
+        if (other.gameObject.name == "Potassium")
+        {
+            Material pota = this.gameObject.GetComponent<Renderer>().material;
+            other.gameObject.SetActive(false);
+           /* pota.SetColor("_SpecColor", Color.yellow);
             pota.SetColor("_TopColor", Color.yellow);
             pota.SetColor("_BottomColor", Color.yellow);
             pota.SetColor("_EdgeColor", Color.yellow);
-            pota.SetColor("_GlowColor", Color.yellow);
+            pota.SetColor("_GlowColor", Color.yellow);*/
+            ChemicalImage[1].SetActive(false);
+            ChemicalImage[0].SetActive(false);
+            ChemicalImage[2].SetActive(true);
+            InfoUI[0].SetActive(false);
+            InfoUI[1].SetActive(true);
+            StartCoroutine(FinalCheck());
+        }
+    }
+
+    IEnumerator FinalCheck() {
+        yield return new WaitForSeconds(5f);
+        if (!TackObjs[0].active && !TackObjs[1].active && !TackObjs[2].active) {
+            Final.SetActive(true);
         }
     }
     
