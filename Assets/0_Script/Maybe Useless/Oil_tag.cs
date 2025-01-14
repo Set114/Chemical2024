fd420e5c110097f012dd1b62a3e15fd4e2acebb8
1masterONE <twehh123@gmail.com>
2025-01-14 18:58:36 +0800

    0114修正 for 測驗2-1
    修改測驗2-1流程架構

---
+++ Assets/0_Script/Maybe Useless/Oil_tag.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+using System;
+
+public class Oil_tag : MonoBehaviour
+{
+    public LevelEndSequence levelEndSequence;
+    public GameObject Oil;
+    public GameObject UI03;
+    public GameObject next_levelTV;
+    public GameObject next_level202;
+    public GameObject next_level201;
+    public GameObject next_levelItem;
+    public string ragwaterTag = "rag_water";
+    public string ragTag = "rag";
+
+    // 引用 SwitchItem
+    public SwitchItem switchItem;
+    private LevelObjManager levelObjManager;
+
+    void Start()
+    {
+        levelObjManager = FindObjectOfType<LevelObjManager>();
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        // 如果碰到標籤為 rag_water 的物體
+        if (other.CompareTag(ragwaterTag))
+        {
+            Debug.Log("碰到rag_water");
+            StartCoroutine(DisableOilWithDelay()); // 啟動協程來延遲禁用 Oil
+        }
+        // 如果碰到標籤為 rag 的物體
+        if (other.CompareTag(ragTag))
+        {
+            Debug.Log("碰到rag");
+            StartCoroutine(ShowAndHideUI()); // 啟動協程來顯示和隱藏 UI
+        }
+    }
+
+    // 協程：延遲 3 秒後禁用 Oil
+    private IEnumerator DisableOilWithDelay()
+    {
+        yield return new WaitForSeconds(3f); // 等待 3 秒
+        if (Oil != null)
+        {
+            Debug.Log("等待三秒");
+            Oil.SetActive(false); // 禁用 Oil
+        }
+
+        // 切換到下一關
+        if (switchItem != null)
+        {
+            switchItem.ChangeLevel();  // 呼叫 SwitchItem 的 ChangeLevel 來顯示下一關的物品
+        }
+
+        if (levelEndSequence != null)
+        {
+           // Debug.Log("場景切換");
+           /*
+            levelEndSequence.EndLevel(false, false, 1f, 1f, 1f, "1", () =>
+            {
+                next_levelItem.SetActive(true);//Item
+                next_level201.SetActive(false);  //2-1
+                next_levelTV.SetActive(true); //TV
+                next_level202.SetActive(true);  //2-2
+            });*/
+            levelObjManager.LevelClear("1", "");
+        }
+    }
+
+    // 協程：顯示 UI，等待 3 秒後隱藏 UI
+    private IEnumerator ShowAndHideUI()
+    {
+        UI03.SetActive(true); // 顯示 UI
+        yield return new WaitForSeconds(3f); // 等待 3 秒
+        UI03.SetActive(false); // 隱藏 UI
+    }
+}
---
+++ Assets/0_Script/TutorialScript/Exam_2_1.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exam_2_1 : MonoBehaviour
+{
+    [Tooltip("盤子內的小蘇打粉")]
+    [SerializeField] private GameObject bakingSodaPowder;
+    [Tooltip("小蘇打粉效果")]
+    [SerializeField] private GameObject particleSystem_SodaPowder;
+    [Tooltip("燒杯內的小蘇打粉")]
+    [SerializeField] private GameObject bakingSodaPowderInWater;
+
+    [Tooltip("抹布")]
+    [SerializeField] private GameObject rag;
+    private MeshRenderer ragMesh;
+    [Tooltip("濕抹布材質")]
+    [SerializeField] private Material wetRagMaterial;
+    [Tooltip("髒抹布材質")]
+    [SerializeField] private Material dirtyRagMaterial;
+
+    [Tooltip("油污")]
+    [SerializeField] private MeshRenderer oilStain;
+
+    [Tooltip("倒入小蘇打速度")]
+    [SerializeField] private float bakingSodaPourSpeed = 1f;
+    [Tooltip("小蘇打溶解速度")]
+    [SerializeField] private float bakingSodaDissolveSpeed = 1f;
+    [Tooltip("油污消失速度")]
+    [SerializeField] private float cleanSpeed = 1f;
+    private int Status = 0;
+
+    private LevelObjManager levelObjManager;
+    private HintManager hintManager;            //管理提示板
+    private QuestionManager questionManager; //管理題目介面
+    private AudioManager audioManager;          //音樂管理
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelObjManager = FindObjectOfType<LevelObjManager>();
+        hintManager = FindObjectOfType<HintManager>();
+        questionManager = FindObjectOfType<QuestionManager>();
+        audioManager = FindObjectOfType<AudioManager>();
+
+        hintManager.gameObject.SetActive(true);
+        hintManager.SwitchStep("E2_1_1");
+
+        ragMesh = rag.GetComponent<MeshRenderer>();
+        bakingSodaPowderInWater.transform.localScale = new Vector3(0f, 0f, 0f);
+    }
+
+    public void Reaction(GameObject sender)
+    {
+        switch (Status)
+        {
+            case 3: //沾濕抹布
+                if (sender.name == "Rag_2-4")
+                {
+                    ragMesh.material = wetRagMaterial;
+                    rag.name = "Rag_Wet_2-4";
+                    rag.GetComponent<CollisionDetection>().targetName = "OilStain";
+                    hintManager.SwitchStep("E2_1_2");
+                }
+                break;
+        }
+    }
+    public void ReactionStay(GameObject sender)
+    {
+        switch (Status)
+        {
+            case 1: //待攪拌完成
+                if (sender.name == "Glass_2-4")
+                {
+                    float size = bakingSodaPowderInWater.transform.localScale.x;
+                    size -= Time.deltaTime * bakingSodaDissolveSpeed;
+                    size = Mathf.Clamp(size, 0f, 1f);
+                    bakingSodaPowderInWater.transform.localScale = new Vector3(size, size, size);
+                    if(size <= 0)
+                    {
+                        //跳出題目一
+                        questionManager.ShowExam(0, this.gameObject);
+                        Status++;
+                    }
+                }
+                break;
+            case 3: //待擦拭桌子
+                if (sender.name == "Rag_Wet_2-4")
+                {
+                    Color oilColor = oilStain.material.color;
+                    oilColor.a -= Time.deltaTime * cleanSpeed;
+                    oilStain.material.color = oilColor;
+                    if (oilColor.a <= 0f)
+                    {
+                        oilColor.a = 0f;
+                        oilStain.material.color = oilColor;
+                        levelObjManager.LevelClear(0);
+                    }
+                }
+                break;
+        }
+    }
+
+    public void PourSodaPowder()
+    {
+        if (Status == 0)
+        {
+            float size = bakingSodaPowderInWater.transform.localScale.x;
+            size += Time.deltaTime * bakingSodaPourSpeed;
+            size = Mathf.Clamp(size, 0f, 1f);
+            bakingSodaPowderInWater.transform.localScale = new Vector3(size, size, size);
+
+            if (size >= 1f)
+            {
+                bakingSodaPowder.SetActive(false);
+                bakingSodaPowderInWater.SetActive(true);
+                particleSystem_SodaPowder.SetActive(false);
+                Status++;
+            }
+        }
+    }
+    //答題完畢
+    public void FinishExam()
+    {
+        Status++;
+    }
+}
