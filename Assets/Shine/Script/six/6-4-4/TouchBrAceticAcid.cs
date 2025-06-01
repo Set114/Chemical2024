a151fd069129ee2e0a49324f7635a57e8dddea52
Shineliu77 <33471613+Shineliu77@users.noreply.github.com>
2025-06-01 17:13:18 +0800

    第六單元測驗動畫

---
+++ Assets/Shine/Script/six/6-4-4/TouchBrAceticAcid.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchBrAceticAcid : MonoBehaviour
+{
+    public GameObject[] CloseObj;
+    public GameObject[] OpenObj;
+    public Animator Ani;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider hit)
+    {
+        if (hit.GetComponent<Collider>().tag == "potassium")
+        {
+            Ani.SetTrigger("Run");
+            for (int i = 0; i < CloseObj.Length; i++)
+            {
+                CloseObj[i].SetActive(false);
+            }
+            for (int i = 0; i < OpenObj.Length; i++)
+            {
+                OpenObj[i].SetActive(true);
+            }
+        }
+    }
+}
